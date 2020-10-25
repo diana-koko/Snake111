ad878f0c3410e7b0919dfc36299cb92f424ab0f0
Unknown <unknown@example.com>
2020-10-25 20:30:10 +0300

    SnakeKeyHandling

--- Snake5/Program.cs
+++ Snake5/Program.cs
@@ -6,7 +6,7 @@ using System.Threading;
 
 namespace Snake5
 {
-    class Program
+	class Program
 	{
 		static void Main(string[] args)
 		{
@@ -27,22 +27,17 @@ namespace Snake5
 			Point p = new Point(4, 5, '*');
 			Snake snake = new Snake(p, 4, Direction.RIGHT);
 			snake.Draw();
-			snake.Move();
-			Thread.Sleep(300);
-			snake.Move();
-			Thread.Sleep(300);
-			snake.Move();
-			Thread.Sleep(300);
-			snake.Move();
-			Thread.Sleep(300);
-			snake.Move();
-			Thread.Sleep(300);
-			snake.Move();
-			Thread.Sleep(300);
-			snake.Move();
-			Thread.Sleep(300);
-			snake.Move();
-			Thread.Sleep(300);
+
+			while (true)
+			{
+				if (Console.KeyAvailable)
+				{
+					ConsoleKeyInfo key = Console.ReadKey();
+					snake.HandleKey(key.Key);
+				}
+				Thread.Sleep(300);
+				snake.Move();
+			}
 		}
 	}
 }
